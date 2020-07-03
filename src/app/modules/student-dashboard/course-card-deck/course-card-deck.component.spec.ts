bda1efc887b87fd4c2ef43ef0a0a814112c77f23
Jeremiah Flaga <j.flaga@arcanys.com>
2020-07-03 10:26:27 +0800

    added tests for displaying courses in dashboard

--- src/app/modules/student-dashboard/course-card-deck/course-card-deck.component.spec.ts
+++ src/app/modules/student-dashboard/course-card-deck/course-card-deck.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CourseCardDeckComponent } from './course-card-deck.component';
+import { By } from '@angular/platform-browser';
 
 describe('CourseCardDeckComponent', () => {
   let component: CourseCardDeckComponent;
@@ -21,5 +22,17 @@ describe('CourseCardDeckComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show empty list when no courses available', () => {
+    const courseCards = fixture.debugElement.queryAll(By.css('app-course-card'));
+    expect(courseCards.length).toBe(0);
+  });
+
+  it('should show one list item when one course is given', () => {
+    component.coursesGroupedByThree = [[{_id: '1', title: '', description: '', image: '', modules: []}]];
+    fixture.detectChanges();
+    const courseCards = fixture.debugElement.queryAll(By.css('app-course-card'));
+    expect(courseCards.length).toBe(1);
   });
 });
